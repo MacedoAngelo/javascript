c58791c63f6ba023cb981822fd8d4e7a2278a4d5
MacedoAngelo <angelostean@outlook.com>
2022-07-28 12:45:20 -0300

    ambiente

---
+++ aula13/ambiente.js
@@ -0,0 +1,41 @@
+var c = 1 
+do  {
+    console.log (`Passo ${c}`)
+    c ++ 
+} while (c <= 6)
+
+
+/*
+while (c <= 6) {
+    console.log (`Passo ${c}`)
+    c ++ 
+}
+*/
+
+
+
+
+/*
+console.log ('Tudo bem ?')
+console.log ('Tudo bem ?')
+console.log ('Tudo bem ?')
+console.log ('Tudo bem ?')
+console.log ('Tudo bem ?')
+console.log ('Tudo bem ?')
+*/
+
+
+var d = 95
+
+do {
+    console.log ('Tudo bem dog')
+    d ++
+} while (d < 100)
+
+
+var e = 1
+
+do {
+    console.log (`Contagem numero ${e}`)
+    e ++
+} while (e <= 10)
